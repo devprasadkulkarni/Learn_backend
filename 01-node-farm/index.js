c89fed89fcec18ac56b5f5ac79bc456452fd2aaa
Prasad Kulkarn <dev.prasad.kulkarni@gmail.com>
2026-06-27 15:05:09 +0530

    building simple api

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -51,12 +51,39 @@ const url = require("url");
 
 // //////////////////////////////
 // ROUTING
+// const server = http.createServer((req, res) => {
+//     const pathName = req.url;
+//     if (pathName === "/" || pathName === "/overview") {
+//         res.end("This is the OVERVIEW");
+//     } else if (pathName === "/product") {
+//         res.end("This is the PRODUCT");
+//     } else {
+//         res.writeHead(404, {
+//             "Content-type": "text/html",
+//             "My-own-header": "Hello World",
+//         });
+//         res.end("Page not found!");
+//     }
+// });
+
+// server.listen(8000, "127.0.0.1", () => {
+//     console.log("Listening to requests on port 8000");
+// });
+
+// //////////////////////////////
+// SIMPLE API
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     if (pathName === "/" || pathName === "/overview") {
         res.end("This is the OVERVIEW");
     } else if (pathName === "/product") {
         res.end("This is the PRODUCT");
+    } else if (pathName === "/api") {
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(data);
     } else {
         res.writeHead(404, {
             "Content-type": "text/html",
