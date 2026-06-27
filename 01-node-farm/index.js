87ecd7472466949a548bc755241d35eed3547c3b
Prasad Kulkarn <dev.prasad.kulkarni@gmail.com>
2026-06-27 10:57:20 +0530

    simple server path routing

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // //////////////////////////////
 // FILES
@@ -40,8 +41,29 @@ const http = require("http");
 
 // //////////////////////////////
 // SERVER
+// const server = http.createServer((req, res) => {
+//     res.end("Hello from the server!");
+// });
+
+// server.listen(8000, "127.0.0.1", () => {
+//     console.log("Listening to requests on port 8000");
+// });
+
+// //////////////////////////////
+// ROUTING
 const server = http.createServer((req, res) => {
-    res.end("Hello from the server!");
+    const pathName = req.url;
+    if (pathName === "/" || pathName === "/overview") {
+        res.end("This is the OVERVIEW");
+    } else if (pathName === "/product") {
+        res.end("This is the PRODUCT");
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "My-own-header": "Hello World",
+        });
+        res.end("Page not found!");
+    }
 });
 
 server.listen(8000, "127.0.0.1", () => {
